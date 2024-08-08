4adaf6fc2ac1a034485ff826254719ac757406e4
salma549 <salmasanubar786@gmail.com>
2024-08-08 11:23:25 +0530

    Frontend

---
+++ frontend/src/customer/components/category/Category.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navigation from './customer/components/Navigation/Navigation.jsx'
+import HomePage from './customer/pages/HomePage/HomePage.jsx'
+import Footer from './customer/components/Footer/Footer.jsx'
+import About from './About/About.jsx'
+import Category from './customer/components/category/Category.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <div>
+        <HomePage />
+        <Category />
+        <About/>
+      </div>
+      <Footer />
+  </div>
+
+  )
+}
+
+export default App
